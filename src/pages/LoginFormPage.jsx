c439b72465201aba4ea1527ab8e061ab5b6e17a5
ChristopherLozada2005 <christopher.lozada@tecsup.edu.pe>
2024-11-09 00:27:26 -0500

    Login con estilos

--- src/pages/LoginFormPage.jsx
+++ src/pages/LoginFormPage.jsx
@@ -1,23 +1,31 @@
 import React from "react"
 import logoUnilink from "../assets/logo-unilink.png"
+import '../css/LoginFormPage.css'
 
 function LoginFormPage(){
 
     return(
         <>
         <nav>
-            <img 
+            <img className="logo"
             src={logoUnilink} 
             alt="Logo" 
-            width={426} 
-            height={240}
             />
         </nav>
         <form action="" className="container">
-            <button>Google</button> <br />
+            <button>Iniciar Sesión con google</button> <br />
             <button>Correo Electrónico</button> <br />
-
             <input type="email" id="email" name="email"  placeholder="Correo" required /> <br />
+            <input type="password" id="password" name="password" placeholder="Contraseña" required  /> <br />
+            <a href="">¿Haz olvidado tu contraseña?</a>
+            <br />
+            <button className="accesButton">Acceso</button> <br />
+            <a href="">
+                ¿No tienes una cuenta?
+                Registrate
+            </a>
+                
+            
 
         </form>
         </>
